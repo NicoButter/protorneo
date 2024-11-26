ef157f6591cda781f20b8c91eaaf5b9decdebc96
Nicolás Butterfield <nicobutter@gmail.com>
2024-11-26 12:40:17 -0300

    general updates

--- dashboards/views.py
+++ dashboards/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from torneos.models import Torneo, AsignacionJuez
 from accounts.models import Juez
 from django.utils import timezone
+from django.contrib.auth.decorators import user_passes_test
 
 
+
+def is_juez(user):
+    return hasattr(user, 'juez')
+
+@user_passes_test(is_juez)
 def dashboard_juez(request):
     juez = request.user.juez  # Suponiendo que el usuario es un juez
     asignaciones = AsignacionJuez.objects.filter(juez=juez)
--- torneos/models.py
+++ torneos/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from pruebas.models import Prueba
-from accounts.models import JefeDeTorneo, Juez  # Asumimos que el jefe de torneo es un tipo de usuario
+from accounts.models import Usuario  # Asumimos que el jefe de torneo es un tipo de usuario
 
 #-------------------------------------------------------------------------------------------------------------------------------------
 
@@ -8,7 +8,7 @@ class Torneo(models.Model):
     nombre = models.CharField(max_length=200)
     fecha = models.DateTimeField()
     lugar = models.CharField(max_length=200)
-    jefe_de_torneo = models.ForeignKey(JefeDeTorneo, on_delete=models.CASCADE)
+    jefe_torneo = Usuario.objects.filter(rol='jefe_torneo')
     pruebas = models.ManyToManyField(Prueba, related_name='torneos', blank=True)
     
     def __str__(self):
@@ -19,7 +19,7 @@ class Torneo(models.Model):
 class AsignacionJuez(models.Model):
     torneo = models.ForeignKey(Torneo, on_delete=models.CASCADE, related_name='asignaciones')
     prueba = models.ForeignKey(Prueba, on_delete=models.CASCADE, related_name='asignaciones')
-    juez = models.ForeignKey(Juez, on_delete=models.CASCADE)
+    juez =  Usuario.objects.filter(rol='juez')
     
     def __str__(self):
         return f"{self.juez} asignado a {self.prueba} en {self.torneo}"
